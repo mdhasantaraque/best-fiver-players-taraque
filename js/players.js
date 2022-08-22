ac8955df61fe6157ff7c88a0961aab6c396ed1b3
Taraque <mtaraque32@gmail.com>
2022-08-23 00:33:19 +0600

    heading added

--- js/players.js
+++ js/players.js
@@ -1,25 +1,25 @@
 const playerArray = [];
 
-function selectPlayer(players){
-  if(players.length>5){
-    alert('You already selected top 5 players');
-  }
-  else{
-    const listBody = document.getElementById('list-body');
-    listBody.innerHTML ='';
+// function selectPlayer(players){
+//   if(players.length>5){
+//     alert('You already selected top 5 players');
+//   }
+//   else{
+//     const listBody = document.getElementById('list-body');
+//     listBody.innerHTML ='';
 
-    for(let i = 0; i < players.length; i++){
-        const name = players[i];
-        const li = document.createElement('li');
-        li.innerHTML = `
-        <li>${i+1} . ${name}</li>
-      `
-      listBody.appendChild(li);
+//     for(let i = 0; i < players.length; i++){
+//         const name = players[i];
+//         const li = document.createElement('li');
+//         li.innerHTML = `
+//         <li>${i+1} . ${name}</li>
+//       `
+//       listBody.appendChild(li);
         
-      }
+//       }
       
-    }
-}
+//     }
+// }
 
 function addToTable(element){
     const playerName = element.parentNode.parentNode.children[0].innerText;
